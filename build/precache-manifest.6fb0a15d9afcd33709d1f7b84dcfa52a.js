520baee547b3f0523f6a3566031d5d6adf405b9f
IneffableImran <Imr16oct92@gmail.com>
2020-01-12 23:29:58 +0530

    updates

---
+++ build/precache-manifest.6fb0a15d9afcd33709d1f7b84dcfa52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6def605a128a0e853857cb803e5c7603",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ab5ca84e5dfdb88943ed",
+    "url": "/calculator/static/css/main.79b00685.chunk.css"
+  },
+  {
+    "revision": "efb3fd9bc3716f58e9c0",
+    "url": "/calculator/static/js/2.ac04fb5c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.ac04fb5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab5ca84e5dfdb88943ed",
+    "url": "/calculator/static/js/main.6f3a3239.chunk.js"
+  },
+  {
+    "revision": "6dd5921c6be255dbaa89",
+    "url": "/calculator/static/js/runtime-main.954e8902.js"
+  }
+]);
